cab201de10dc4e5205aa692f456417ac534803a3
neha <nehha1124@gmail.com>
2022-03-21 11:54:26 +0530

    git step

--- root.js
+++ root.js
@@ -1,2 +1,10 @@
 console.log("hello world")
  console.log(" practice of git")
+
+
+
+ //steps
+//  step 1: first i make a File
+//  step 2: then i add 
+//  step 3:commmit my File
+//  step 4:using command git push origin MediaStream then i push my code in githun
