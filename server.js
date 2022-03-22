62de83cab984e571ec3d0354c3e5bb6cdcc429a7
neha <nehha1124@gmail.com>
2022-03-22 16:27:16 +0530

    this is server file where i make all the server with html file and api with using http

---
+++ server.js
@@ -0,0 +1,23 @@
+// server
+/*var http=require('http')
+var server=http.createServer()
+server.listen(3000,console.log("hello"))*/
+
+
+// server with http with api
+/*var http=require('http')
+var server=http.createServer(function(req,res){
+    res.writeHead(200,{"content-type":"application-json"})
+    res.write("hello")
+    res.end();
+}).listen(3000)*/
+
+
+// sever where we write html 
+ /*var http=require('http')
+ var server=http.createServer(function(req,res){
+     res.writeHead(200,{"content-type":"text/html"})
+     res.write("<input type='text'/>")
+     res.end();
+ }).listen(3000)*/
+
